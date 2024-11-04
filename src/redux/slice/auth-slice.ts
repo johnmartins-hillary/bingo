73e7d9e29f9acb23bf156a113999e1150d12910d
johnmartins-hillary <ekwealorhillary@gmail.com>
2024-11-04 17:56:04 +0100

    initialized bingo project

---
+++ src/redux/slice/auth-slice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface AuthState {
+    user: any;
+    isAuthenticated: boolean;
+}
+
+const initialState: AuthState = {
+    user: null,
+    isAuthenticated: false
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login(state, action) {
+            state.user = action.payload;
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.user = null;
+            state.isAuthenticated = false;
+        },
+        setUser(state, action) {
+            state.user = action.payload;
+        }
+    }
+});
+
+export const { login, logout, setUser } = authSlice.actions;
+export default authSlice.reducer;
